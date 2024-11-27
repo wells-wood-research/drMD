b89e8c6f3e7f73f5d59c18fbd3d5aa3a529bcb46
ESPhoenix <e.s.phoenix@hotmail.co.uk>
2024-11-27 16:28:05 +0000

    pip updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='drMD',
-    version = '1.0.0',
+    version = '1.0.1',
     description='drMD: Molecular Dynamics for Protein Scientists',
     author="Dr Eugene Shrimpton-Phoenix",
     author_email="eshrimpt@ed.ac.uk",
